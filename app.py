791bb3ad4d0e219f5ce4ab16ba82e054948d0662
CryAndRRich <willshane2006@gmail.com>
2024-12-04 09:31:11 +0700

    Add return route

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
 @app.route('/')
 def home():
@@ -9,17 +10,24 @@ def home():
 @app.route('/variations', methods = ['POST'])
 def variations():
     msg = {'player1': request.form['player1'],
-           'player2': request.form['player2']}
+            'player2': request.form['player2']}
+
+    session['message'] = msg
+
     return render_template('variations.html', msg = msg)
+
+@app.route('/variations/back', methods=['POST'])
+def variations_back():
+    return render_template('variations.html', msg = session.get('message'))
 
 @app.route('/basic')
 def basic():
-    return "hello basic"
+    return render_template('variants_template/basic.html', msg = session.get('message'))
 
 @app.route('/hexagonal')
 def hexagonal():
-    return render_template('hexagonal.html')
+    return render_template('variants_template/hexagonal.html', msg = session.get('message'))
 
 @app.route('/qubic')
 def qubic():
-    return render_template('qubic.html')
+    return render_template('variants_template/qubic.html', msg = session.get('message'))
